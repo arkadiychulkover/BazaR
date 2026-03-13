50b65493eadba8ac28d80c5d61fe0c9388c1411e
Arkadii <arkadiychulkov11@gmail.com>
2026-03-13 19:09:32 +0200

    searchRecsFirst

---
+++ BazaR/DTO/SearchBarBrandDTO.cs
@@ -0,0 +1,16 @@
+﻿using BazaR.Models;
+
+namespace BazaR.DTO
+{
+    public class SearchBarBrandDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public SearchBarBrandDTO(Brand b) 
+        {
+            Id = b.Id;
+            Name = b.Name;
+        }
+    }
+}
