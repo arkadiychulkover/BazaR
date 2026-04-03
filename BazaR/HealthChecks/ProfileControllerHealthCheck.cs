ec84d7eb1252e8e21d23e42fc398d97ae03834b9
Arkadii <arkadiychulkov11@gmail.com>
2026-04-04 00:57:53 +0300

    Health checks

---
+++ BazaR/HealthChecks/ProfileControllerHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BazaR.HealthChecks
+{
+    public class ProfileControllerHealthCheck : IHealthCheck
+    {
+        private readonly IHttpClientFactory _httpClientBuilder;
+        public ProfileControllerHealthCheck(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientBuilder = httpClientFactory;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var client = _httpClientBuilder.CreateClient();
+                var response = await client.GetAsync("/Profile/Profile");
+                if (response.IsSuccessStatusCode)
+                {
+                    return HealthCheckResult.Healthy("Profile controller works");
+                }
+                return HealthCheckResult.Degraded("Profile controller do not works");
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("Totaly unhealthy");
+            }
+        }
+    }
+}
---
+++ BazaR/HealthChecks/SiteControllerHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BazaR.HealthChecks
+{
+    public class SiteControllerHealthCheck : IHealthCheck
+    {
+        private readonly IHttpClientFactory _httpClientBuilder;
+        public SiteControllerHealthCheck(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientBuilder = httpClientFactory;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default) 
+        {
+            try 
+            { 
+                var client = _httpClientBuilder.CreateClient();
+                var response = await client.GetAsync("/SiteController/Index");
+                if (response.IsSuccessStatusCode)
+                {
+                    return HealthCheckResult.Healthy("Site controller works");
+                }
+                return HealthCheckResult.Degraded("Site controller do not works");
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("Totaly unhealthy");
+            }
+        }
+    }
+}
