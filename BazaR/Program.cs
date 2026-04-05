babce2018e48b3f4e7e4921d86c94ac5d46b6be9
Arkadii <arkadiychulkov11@gmail.com>
2026-04-05 10:49:50 +0300

    deployment

--- BazaR/Program.cs
+++ BazaR/Program.cs
@@ -9,15 +9,15 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
+Console.WriteLine("LOG");
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews(o =>
 {
     o.Filters.Add<UserContextFilter>();
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
 
 builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
 {
@@ -84,9 +84,14 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    Console.WriteLine("Scope");
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+
     var dbMaker = scope.ServiceProvider.GetRequiredService<DbMaker>();
     await dbMaker.MakeAsync();
 }
+Console.WriteLine("AfterScope");
 
 if (!app.Environment.IsDevelopment())
 {
